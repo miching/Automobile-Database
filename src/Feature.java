0f0dddba79caaf557074dfc75f36de7ca886fa75
Michael Ching <michaeljune101@yahoo.com>
2022-05-08 02:01:52 -0700

    Added necessary functions for Feature

--- src/Feature.java
+++ src/Feature.java
@@ -19,5 +19,57 @@ public class Feature
     private Set<Trim> trimFeature;
 
     private Set<Model> modelFeature;
+
+    public Feature()
+    {
+
+
+
+    }
+
+    public Feature(String name)
+    {
+
+        this.name = name;
+
+    }
+
+    public int getFeatureID() {
+        return featureID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Package> getPackageFeature() {
+        return packageFeature;
+    }
+
+    public void setPackageFeature(Set<Package> packageFeature) {
+        this.packageFeature = packageFeature;
+    }
+
+    public Set<Trim> getTrimFeature() {
+        return trimFeature;
+    }
+
+    public void setTrimFeature(Set<Trim> trimFeature) {
+        this.trimFeature = trimFeature;
+    }
+
+    public Set<Model> getModelFeature() {
+        return modelFeature;
+    }
+
+    public void setModelFeature(Set<Model> modelFeature) {
+        this.modelFeature = modelFeature;
+    }
+
+    
     
 }
