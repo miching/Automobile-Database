6d76e33a7d27af7f5a59bc861945218573a2d945
Michael Ching <michaeljune101@yahoo.com>
2022-05-08 01:59:41 -0700

    Added necessary functions for AvailablePackage

--- src/AvailablePackage.java
+++ src/AvailablePackage.java
@@ -24,6 +24,59 @@ public class AvailablePackage
     //Bidirectional Many-to-Many with automobile
     @ManyToMany(mappedBy = "chosenPackages")
     private Set<Automobile> chosenPackages;
+
+    public AvailablePackage() 
+    {
+
+
+
+    }
+
+    public AvailablePackage(Trim trim, Package package1, int cost) {
+        this.trim = trim;
+        this.package1 = package1;
+        this.cost = cost;
+    }
+
+    public int getAvailableID() {
+        return availableID;
+    }
+
+    public Trim getTrim() {
+        return trim;
+    }
+
+    public void setTrim(Trim trim) {
+        this.trim = trim;
+    }
+
+    public Package getPackage1() {
+        return package1;
+    }
+
+    public void setPackage1(Package package1) {
+        this.package1 = package1;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Set<Automobile> getChosenPackages() {
+        return chosenPackages;
+    }
+
+    public void setChosenPackages(Set<Automobile> chosenPackages) {
+        this.chosenPackages = chosenPackages;
+    }
+
+    
+
+    
     
     
 }
